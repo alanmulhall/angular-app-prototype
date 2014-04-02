407794c55efaeadc3454b45357ec10acb9fc20e7
Alan Mulhall <alanmulhall@hotmail.com>
2014-04-01 20:09:34 -0700

    initial commit

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('firstAngularAppApp')
+  .controller('AboutCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/contact.js
@@ -0,0 +1,12 @@
+'use strict';
+
+angular.module('firstAngularAppApp')
+  .controller('ContactCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+
+    $scope.test = 'This works Alan';
+  });
